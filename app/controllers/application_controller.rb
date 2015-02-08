d7f99eed3b8cc6360d7e75cc673650068cc56455
jameshedaweng <heda.weng@gmail.com>
2015-02-08 22:22:18 +0100

    fixed locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
+  before_action :set_locale
 
   def set_locale
     # update sessions if passed
     session[:locale] = params[:locale] if params[:locale]
 
-    # set locale based on user settings
-    I18n.locale = extract_locale_from_accept_language_header
+    # set locale based on sessions or default
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header
+    #I18n.locale = extract_locale_from_accept_language_header
   end
 
   private
-    def extract_locale_from_accept_language_header
-      case request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-        when 'es'
-          'es'
-        when 'zh'
-          'zh'
-        else
-          'en'
-      end
+
+  def extract_locale_from_accept_language_header
+    case request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      when 'es'
+        'es'
+      when 'zh'
+        'zh'
+      else
+        'en'
     end
+  end
 end
