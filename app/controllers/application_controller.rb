d3e72617feec8beae3744306a7e149dffaa29613
jameshedaweng <heda.weng@gmail.com>
2015-02-08 22:08:11 +0100

    show language according to user settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,19 @@ class ApplicationController < ActionController::Base
     # update sessions if passed
     session[:locale] = params[:locale] if params[:locale]
 
-    # set locale based on sessions or default
-    I18n.locale = session[:locale] || I18n.default_locale
+    # set locale based on user settings
+    I18n.locale = extract_locale_from_accept_language_header
   end
+
+  private
+    def extract_locale_from_accept_language_header
+      case request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+        when 'es'
+          'es'
+        when 'zh'
+          'zh'
+        else
+          'en'
+      end
+    end
 end
