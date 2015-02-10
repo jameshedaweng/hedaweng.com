ed2118f76838a7eaf80350a034ffdc86fec70438
jameshedaweng <heda.weng@gmail.com>
2015-02-10 04:08:02 +0100

    make error page prettier

--- app/assets/javascripts/static_pages/error_pages.js
+++ app/assets/javascripts/static_pages/error_pages.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+	centerError();
 	$(".error-body").fadeIn(1000);
+
+	$(window).resize(function() {
+		centerError();
+	});
 });
+
+function centerError(){
+	imageH = 300;
+	if (window.innerWidth * 0.8 < imageH * 2){
+		imageH = window.innerWidth * 0.8 / 2;
+	}
+	paddingTop = (window.innerHeight - imageH)/2;
+	$(".error-body").css("padding-top", paddingTop);
+};
