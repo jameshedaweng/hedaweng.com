4f237ae90a703319d050fa71bac8628c4c7258de
jameshedaweng <heda.weng@gmail.com>
2015-02-10 01:38:26 +0100

    ie redirect test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HedawengCom::Application.routes.draw do
   get "static_pages/home"
-  get "static_pages/ie"
+  get "static_pages/ie", constraints: { subdomain: 'ie' }
   root :to => "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
